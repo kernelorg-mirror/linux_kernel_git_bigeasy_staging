f01ec48913e85831d45c5295e02f1d3eb6921b6a
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2018-06-07 16:29:48 +0200

    ALSA: usb: caiaq: use usb_fill_iso_urb()
    Using usb_fill_iso_urb() helps to find code which initializes an
    URB. A grep for members of the struct (like ->complete) reveal lots
    of other things, too.
    
    Cc: Daniel Mack <zonque@gmail.com>
    Cc: Jaroslav Kysela <perex@perex.cz>
    Cc: Takashi Iwai <tiwai@suse.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- sound/usb/caiaq/audio.c
+++ sound/usb/caiaq/audio.c
@@ -719,7 +719,7 @@ static void write_completed(struct urb *urb)
 
 static struct urb **alloc_urbs(struct snd_usb_caiaqdev *cdev, int dir, int *ret)
 {
-	int i, frame;
+	int i;
 	struct urb **urbs;
 	struct usb_device *usb_dev = cdev->chip.dev;
 	unsigned int pipe;
@@ -735,37 +735,27 @@ static struct urb **alloc_urbs(struct snd_usb_caiaqdev *cdev, int dir, int *ret)
 	}
 
 	for (i = 0; i < N_URBS; i++) {
+		void *buf;
+
 		urbs[i] = usb_alloc_urb(FRAMES_PER_URB, GFP_KERNEL);
 		if (!urbs[i]) {
 			*ret = -ENOMEM;
 			return urbs;
 		}
 
-		urbs[i]->transfer_buffer =
-			kmalloc_array(BYTES_PER_FRAME, FRAMES_PER_URB,
-				      GFP_KERNEL);
-		if (!urbs[i]->transfer_buffer) {
+		buf = kmalloc_array(BYTES_PER_FRAME, FRAMES_PER_URB,
+				    GFP_KERNEL);
+		if (!buf) {
 			*ret = -ENOMEM;
 			return urbs;
 		}
 
-		for (frame = 0; frame < FRAMES_PER_URB; frame++) {
-			struct usb_iso_packet_descriptor *iso =
-				&urbs[i]->iso_frame_desc[frame];
-
-			iso->offset = BYTES_PER_FRAME * frame;
-			iso->length = BYTES_PER_FRAME;
-		}
-
-		urbs[i]->dev = usb_dev;
-		urbs[i]->pipe = pipe;
-		urbs[i]->transfer_buffer_length = FRAMES_PER_URB
-						* BYTES_PER_FRAME;
-		urbs[i]->context = &cdev->data_cb_info[i];
-		urbs[i]->interval = 1;
-		urbs[i]->number_of_packets = FRAMES_PER_URB;
-		urbs[i]->complete = (dir == SNDRV_PCM_STREAM_CAPTURE) ?
-					read_completed : write_completed;
+		usb_fill_iso_urb(urbs[i], usb_dev, pipe, buf,
+				 FRAMES_PER_URB * BYTES_PER_FRAME,
+				 (dir == SNDRV_PCM_STREAM_CAPTURE) ?
+				 read_completed : write_completed,
+				 &cdev->data_cb_info[i], 1, FRAMES_PER_URB,
+				 BYTES_PER_FRAME);
 	}
 
 	*ret = 0;
